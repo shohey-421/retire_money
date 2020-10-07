04db2c9f737560c3d022a927f60b488803a484a7
shohey-421 <shohey-421@users.noreply.github.com>
2020-10-07 16:28:33 +0900

    トップへ戻るボタンの追加

--- app/javascript/btn.js
+++ app/javascript/btn.js
@@ -6,8 +6,20 @@ $(function(){
 
   $('.create-btn').on('click',function(){
     $('.table-wrapper').fadeIn();
-    $('body,html').animate({scrollTop: 1200}, 500);
+    $('body,html').animate({scrollTop: 5000}, 500);
     return false;
-
   }); 
+
+  $('#page_top').hide();
+  $(window).scroll(function () {
+     if ($(this).scrollTop() > 100) {
+      $('#page_top').fadeIn();
+     } else {
+      $('#page_top').fadeOut();
+     }
+  });
+  $('#page_top').click(function () {
+     $('body, html').animate({ scrollTop: 0 }, 500);
+     return false;
+  });
 });
